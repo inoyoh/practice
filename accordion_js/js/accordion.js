b69bd5e222f937500c83b4ab4f0c7b4becb370bc
yohei <inoyoh@yoheinoMacBook-Pro.local>
2018-02-25 16:22:50 +0900

    コメント追記。

--- accordion_js/js/accordion.js
+++ accordion_js/js/accordion.js
@@ -1,6 +1,9 @@
 $(function(){
+  // 一旦コンテンツを隠す
   $(`.panel__content`).hide();
+  // タイトルをクリックした際の挙動
   $(`.panel__title`).click(function(){
+    // this(タイトルをクリックした要素）の次の要素を表示する
     $(this).next().slideToggle(500);
   });
 });
